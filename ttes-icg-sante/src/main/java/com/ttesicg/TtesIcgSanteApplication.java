dca35b50a93540929ee0f141aac921db629019e0
unknown <tsakengjr@gmail.com>
2026-09-11 17:20:36 +0100

    TtesIcgSanteApplication

--- ttes-icg-sante/src/main/java/com/ttesicg/TtesIcgSanteApplication.java
+++ ttes-icg-sante/src/main/java/com/ttesicg/TtesIcgSanteApplication.java
@@ -1,27 +1,12 @@
 package com.ttesicg;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class TtesIcgSanteApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TtesIcgSanteApplication.class, args);
-    }
-
-    @Bean
-    CommandLineRunner testEnv(Environment env) {
-        return args -> {
-            String pwd = env.getProperty("MAIL_PASSWORD");
-            System.out.println("========== TEST .ENV ==========");
-            System.out.println("MAIL_PASSWORD = [" + pwd + "]");
-            System.out.println("Longueur = " + pwd.length());
-            System.out.println("Codes = " + java.util.Arrays.toString(pwd.chars().toArray()));
-            System.out.println("================================");
-        };
     }
 }

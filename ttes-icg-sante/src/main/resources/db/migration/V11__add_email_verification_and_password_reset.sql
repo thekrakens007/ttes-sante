5c5dfed22f99fc53685e40a68dae0cd4cb038041
unknown <tsakengjr@gmail.com>
2026-09-13 01:02:38 +0100

    fix: restore applied Flyway migration V11

--- ttes-icg-sante/src/main/resources/db/migration/V11__add_email_verification_and_password_reset.sql
+++ ttes-icg-sante/src/main/resources/db/migration/V11__add_email_verification_and_password_reset.sql
@@ -1,8 +1,8 @@
-ALTER TABLE users ADD COLUMN email_verified BOOLEAN NOT NULL DEFAULT FALSE;
+﻿ALTER TABLE users ADD COLUMN email_verified BOOLEAN NOT NULL DEFAULT FALSE;
 ALTER TABLE users ADD COLUMN verification_token VARCHAR(255);
 ALTER TABLE users ADD COLUMN verification_token_expiry TIMESTAMP;
 ALTER TABLE users ADD COLUMN reset_password_token VARCHAR(255);
 ALTER TABLE users ADD COLUMN reset_password_token_expiry TIMESTAMP;
 
--- Les comptes déjà existants ne doivent pas être bloqués
+-- Les comptes d├®j├á existants ne doivent pas ├¬tre bloqu├®s
 UPDATE users SET email_verified = TRUE WHERE email_verified = FALSE;
